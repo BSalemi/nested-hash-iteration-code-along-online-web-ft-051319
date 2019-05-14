0fc4e98fb9aaa747187064c8a7f132f22d4ef018
Brian Salemi <briandsalemi@gmail.com>
2019-05-14 14:15:09 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -18,7 +18,7 @@ def remove_strawberry(contacts)
   contacts["Freddy Mercury"].each do |freddy_facts, freddy_data|
     binding.pry
     if freddy_facts == :favorite_ice_cream_flavors
-      :favorite_ice_cream_flavors.delete("strawberry") if :favorite_ice_cream_flavors.include?("strawberry")
+      freddy_data.delete("strawberry") if freddy_data.include?("strawberry")
     end
   end
 end
